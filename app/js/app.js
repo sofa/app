eb8df86e386b047be619349783784ed582c1dbad
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2013-03-21 17:32:05 +0100

    use overthrow.js to support overflow on Android 2.3

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 angular.module('CouchCommerceApp', []).
   config(['$routeProvider', function($routeProvider) {
