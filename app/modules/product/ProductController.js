52949fb72889d4eb772bbf769beb905f6c78a32b
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2013-04-17 23:07:58 +0200

    feature:(product): multiple price info / add to cart buttons

--- app/modules/product/ProductController.js
+++ app/modules/product/ProductController.js
@@ -7,11 +7,19 @@ angular
         '$scope', '$routeParams', '$location', 'couchService', 'productService', 'product',
         function ProductController($scope, $routeParams, $location, couchService, productService, product) {
 
+            $scope.ui = {
+                PRICE_INFO_AND_ADD_TO_CART: 'modules/product/price-info-and-add-to-cart.tpl.html'
+            };
+
             $scope.productService = productService;
 
             $scope.product = product;
 
             //guess it would make sense if we had lang on the $routeScope
             $scope.lang = cc.Lang;
+
+            $scope.addToBasket = function(product){
+                alert('not implemented');
+            };
         }
     ]);
