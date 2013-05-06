e5ffe9394bfeec036d6a1a237bf2f9708d04b5b3
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2013-05-06 21:42:47 +0200

    reworked zippy directive to allow optional bindings

--- sdk/src/directives/ccZippy/ccZippy.js
+++ sdk/src/directives/ccZippy/ccZippy.js
@@ -2,42 +2,49 @@
 
 angular.module('sdk.directives.ccZippy', []);
 
-/**
- * this is a generic directive that creates an view with 
- * optional fixed header and toolbars
- *
- */
-
 angular.module('sdk.directives.ccZippy')
     .directive('ccZippy', function() {
+
+        var defaultIfUndefined = function(scope, property, defaultVal){
+            return scope[property] = scope[property] === undefined ? defaultVal : scope[property];
+        };
+
         return {
             restrict: 'E',
             replace: true,
             transclude: true,
             scope: {
-                caption: '=',
+                caption: '=?',
+                opened: '=?'
             },
             templateUrl: '../sdk/src/directives/ccZippy/cczippy.tpl.html',
             link: function(scope, element, attrs){
                 var $element = $(element[0]),
                     $caption = $element.children('.cc-zippy-caption').first(),
                     $icon = $element.find('.cc-zippy-icon').first(),
-                    opened = true,
                     openedIconClass = 'icon-chevron-up',
                     closedIconClass = 'icon-chevron-down';
 
-                var toggle = function(){
-                    opened = !opened;
+                defaultIfUndefined(scope, 'caption', 'default');
+                defaultIfUndefined(scope, 'opened', false);
+
+                var setOpen = function(opened){
                     $element.removeClass(opened ? 'cc-zippy-closed' : 'cc-zippy-opened');
                     $element.addClass(opened ? 'cc-zippy-opened' : 'cc-zippy-closed');
                     $icon.removeClass(opened ? closedIconClass : openedIconClass);
                     $icon.addClass(opened ? openedIconClass : closedIconClass);
                 };
 
+                var toggle = function(){
+                    scope.opened = !scope.opened;
+                    setOpen(scope.opened);
+                };
+
                 $caption.bind('click', toggle);
 
-                toggle();
+                scope.$watch('opened', setOpen);
 
+                setOpen(scope.opened);
             }
         };
     });
