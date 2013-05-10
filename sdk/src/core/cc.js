b33038fd1cd45e4abb1ab867ee65b16ea08f5186
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2013-05-10 11:15:44 +0200

    feat(core): added an inheritance helper

--- sdk/src/core/cc.js
+++ sdk/src/core/cc.js
@@ -2,20 +2,20 @@
 
 var cc = {};
 
-    /**
-     * Creates the given namespace within the cc namespace.
-     * The methor returns an object that contains meta data
-     *
-     * - targetParent (object)
-     * - targetName (string)
-     * - bind (function) : a convenient function to bind
-                           a value to the namespace
-     * 
-     * Options:
-     * 
-     *   - `namespaceString` e.g. 'cc.services.FooService'
-     * 
-     */
+/**
+ * Creates the given namespace within the cc namespace.
+ * The method returns an object that contains meta data
+ *
+ * - targetParent (object)
+ * - targetName (string)
+ * - bind (function) : a convenient function to bind
+                       a value to the namespace
+ * 
+ * Options:
+ * 
+ *   - `namespaceString` e.g. 'cc.services.FooService'
+ * 
+ */
 
 cc.namespace = function (namespaceString) {
     var parts = namespaceString.split('.'), parent = cc, i;
@@ -55,4 +55,45 @@ cc.define = function(namespace, fn){
     cc.namespace(namespace)
       .bind(fn);
 };
+
+/**
+ * Sets up an inheritance chain between two objects
+ * https://github.com/isaacs/inherits/blob/master/inherits.js
+ * Can be used like this:
+ *
+ *   function Child () {
+ *    Child.super.call(this)
+ *    console.error([this
+ *                  ,this.constructor
+ *                  ,this.constructor === Child
+ *                  ,this.constructor.super === Parent
+ *                  ,Object.getPrototypeOf(this) === Child.prototype
+ *                  ,Object.getPrototypeOf(Object.getPrototypeOf(this))
+ *                   === Parent.prototype
+ *                  ,this instanceof Child
+ *                  ,this instanceof Parent])
+ *  }
+ *  function Parent () {}
+ *  inherits(Child, Parent)
+ *  new Child
+ *
+ */
+
+
+cc.inherits = function (c, p, proto) {
+    //this code uses a shitty form of semicolon less
+    //writing. We just copied it from:
+    //https://github.com/isaacs/inherits/blob/master/inherits.js
+    proto = proto || {}
+    var e = {}
+    ;[c.prototype, proto].forEach(function (s) {
+        Object.getOwnPropertyNames(s).forEach(function (k) {
+            e[k] = Object.getOwnPropertyDescriptor(s, k)
+        })
+    })
+    c.prototype = Object.create(p.prototype, e)
+    c.super = p
+};
+
+
 
