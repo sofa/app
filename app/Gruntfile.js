4dd82210983e1b4d7633011ecf01d7ea968c7a43
Julien Bouquillon <julien@bouquillon.com>
2013-06-28 00:56:54 +0200

    chore(grunt): add Gruntfile.js in /app
     - add watch for SASS
     - add connect for a test server
     - part of ticket #5

---
+++ app/Gruntfile.js
@@ -0,0 +1,52 @@
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // uglify: {
+    //   options: {
+    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    //   },
+    //   build: {
+    //     src: 'modules/<%= pkg.name %>.js',
+    //     dest: 'build/<%= pkg.name %>.min.js'
+    //   }
+    // },
+    sass: {
+      dev: {
+        options: {
+          // nested (default), compact, compressed, or expanded.
+          style: 'expanded'
+        },
+        files: {
+          'css/app.css': 'scss/app.scss'
+        }
+      }
+    },
+    watch: {
+      sass: {
+        files: ['scss/**/*.scss', '../sdk/src/styling/**/*.scss'],
+        tasks: ['sass']
+      }
+    },
+     connect: {
+      server: {
+        options: {
+          port: 9191,
+          base: '..',
+          keepalive: true,
+          hostname: '0.0.0.0'
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['sass']);
+
+  //grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+};
