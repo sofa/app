2c300f65ce4860039d5b735eb1af638b540b1a1d
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2013-10-21 15:29:24 +0200

    chore(*): refactored template so that MainController is no longer needed

--- app/modules/common/TemplateService.js
+++ app/modules/common/TemplateService.js
@@ -2,5 +2,6 @@ angular
     .module('CouchCommerceApp')
     .constant('templateService',{
         PRODUCT_BUY_BOX: 'modules/product/product-buy-box.tpl.html',
-        TOTALBOX: 'modules/common/totalbox/totalbox.tpl.html'
+        TOTALBOX: 'modules/common/totalbox/totalbox.tpl.html',
+        HEADER: 'modules/common/header/defaultheader.tpl.html'
     });
