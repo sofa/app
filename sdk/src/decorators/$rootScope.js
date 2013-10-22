77f089e9538d385d7786ec996b32c00dff29bb45
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2013-10-23 01:18:23 +0200

    fix(): make  work with isolated scopes

--- sdk/src/decorators/$rootScope.js
+++ sdk/src/decorators/$rootScope.js
@@ -15,7 +15,7 @@ angular.module('sdk.decorators.$rootScope', []);
                 
                 //Read this for more info:
                 //http://stackoverflow.com/questions/11252780/whats-the-correct-way-to-communicate-between-controllers-in-angularjs/19498009#19498009
-                $delegate.$onRootScope = function(name, listener){
+                $delegate.constructor.prototype.$onRootScope = function(name, listener){
                     var unsubscribe = $delegate.$on(name, listener);
                     this.$on('$destroy', unsubscribe);
                 };
