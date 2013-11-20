ba4dcb011ada7df703835ba7f8ae243f94c62c10
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2013-11-20 14:23:27 +0100

    fix(CategoryController): make sure to always fetch the category through the couchService

--- app/modules/categories/CategoryController.js
+++ app/modules/categories/CategoryController.js
@@ -19,11 +19,24 @@ angular
 
                 selectionService.select($stateParams.category, angular.element($event.currentTarget));
 
-                if (!category.children){
-                    navigationService.navigateToProducts(category.urlId);
-                } else {
-                    navigationService.navigateToCategory(category.urlId);
-                }
+                //even if we have the category already provided by the function parameter,
+                //we have to fetch it again via the couchService. The reason for that is, that
+                //it might be a leaf with no children but it's just an alias to a category
+                //which does have children. In this case, we want to get to the real category
+                //which has the children.
+
+                //in 99% of the cases we will just get the same category returned by the couchService
+                //as the one we got via the function parameter but for such alias corner cases it's important
+                //to retrieve it through the couchService.
+                couchService
+                    .getCategory(category.urlId)
+                    .then(function(realCategory){
+                        if (!realCategory.children){
+                            navigationService.navigateToProducts(realCategory.urlId);
+                        } else {
+                            navigationService.navigateToCategory(realCategory.urlId);
+                        }
+                    });
             };
 
             if (!category.children){
