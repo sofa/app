f46c3dc7c68d36fc6877c03bc06207130a120faf
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2013-12-12 16:37:42 +0100

    (header/search): apply new file naming conventions

--- app/modules/common/TemplateService.js
+++ app/modules/common/TemplateService.js
@@ -4,8 +4,8 @@ angular
         PRODUCT_BUY_BOX: 'modules/product/product-buy-box.tpl.html',
         TOTALBOX: 'modules/common/totalbox/totalbox.tpl.html',
         HEADER: 'modules/common/header/cc-header.tpl.html',
-        SEARCH_BOX: 'modules/search/search-box.tpl.html',
-        SEARCH_RESULT: 'modules/search/search-result.tpl.html',
+        SEARCH_BOX: 'modules/search/cc-search-box.tpl.html',
+        SEARCH_RESULT: 'modules/search/cc-search-result.tpl.html',
         SHELL: 'modules/common/shell/shell.tpl.html',
         '404': 'modules/common/404/cc-404.tpl.html',
         '404_PRODUCTS': 'modules/common/404/cc-404-products.tpl.html'
