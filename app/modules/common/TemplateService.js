bd1ca5564d4114f5514f072f1bb5939e584b0054
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2013-12-12 16:51:04 +0100

    (totalbox): apply new file naming/theming conventions

--- app/modules/common/TemplateService.js
+++ app/modules/common/TemplateService.js
@@ -2,7 +2,7 @@ angular
     .module('CouchCommerceApp')
     .constant('templateService',{
         PRODUCT_BUY_BOX: 'modules/product/product-buy-box.tpl.html',
-        TOTALBOX: 'modules/common/totalbox/totalbox.tpl.html',
+        TOTALBOX: 'modules/common/totalbox/cc-cart-total-panel.tpl.html',
         HEADER: 'modules/common/header/cc-header.tpl.html',
         SEARCH_BOX: 'modules/search/cc-search-box.tpl.html',
         SEARCH_RESULT: 'modules/search/cc-search-result.tpl.html',
