24446f2e55dfea7b0da506e44c583721a7c9c50c
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2013-12-17 23:24:39 +0100

    fix(): fix outdated comment

--- sdk/dist/cc.angular.js
+++ sdk/dist/cc.angular.js
@@ -3266,15 +3266,17 @@ angular.module('sdk.decorators.$rootScope', []);
             $provide.decorator('$rootScope', ['$delegate', function($delegate){
 
 
-                //we monkey patch the $rootScope to provide a $saveOn method that
-                //just works like the $on method but takes an additional parameter
-                //where a scope can be provided to listen for $destroy event in order
-                //to than automatically unregister the listener.
-                //That comes in handy when the $rootScope is treated as EventBus
-                //and is used for all inter app communication.
+                // we monkey patch the $rootScope to provide a $onRootScope method that
+                // just works like the $on method but subscribes to events directly emitted
+                // on the $rootScope.
+                // While one can directly bind to events emitted on the $rootScope even without
+                // such a `$onRootScope` method, this method makes sure that events are automatically
+                // unbound when the local scope gets destroyed.
+                // This comes in handy when the $rootScope is treated as EventBus
+                // and is used for all inter app communication.
                 
-                //Read this for more info:
-                //http://stackoverflow.com/questions/11252780/whats-the-correct-way-to-communicate-between-controllers-in-angularjs/19498009#19498009
+                // Read this for more info:
+                // http://stackoverflow.com/questions/11252780/whats-the-correct-way-to-communicate-between-controllers-in-angularjs/19498009#19498009
 
                 Object.defineProperty($delegate.constructor.prototype, '$onRootScope', {
                     value: function(name, listener){
