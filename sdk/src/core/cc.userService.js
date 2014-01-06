1991de6ce895fb0a974bfd628dd322fb3d58118c
Pascal Precht <pascal.precht@googlemail.com>
2014-01-06 16:41:25 +0100

    docs(cc.UserService): adds docs for cc.UserService

--- sdk/src/core/cc.userService.js
+++ sdk/src/core/cc.userService.js
@@ -1,3 +1,11 @@
+/**
+ * @name UserService
+ * @namespace cc.UserService
+ *
+ * @description
+ * User related service that let's you access things like billing addresses, shipping
+ * addresses and similar things.
+ */
 cc.define('cc.UserService', function(storageService, configService){
 
     'use strict';
@@ -8,7 +16,13 @@ cc.define('cc.UserService', function(storageService, configService){
         STORE_SHIPPING_ADDRESS_KEY = STORE_PREFIX + 'shippingAddress';
 
     /**
-     * Gets the invoice address for the user
+     * @method getInvoiceAddress
+     * @memberof cc.UserService
+     *
+     * @description
+     * Gets the invoice address for the user.
+     *
+     * @return {object} address Address object.
      */
     self.getInvoiceAddress = function(){
         var address = storageService.get(STORE_INVOICE_ADDRESS_KEY);
@@ -25,14 +39,26 @@ cc.define('cc.UserService', function(storageService, configService){
     };
 
     /**
-     * Creates/Updates the invoice address for the user
+     * @method updateInvoiceAddress
+     * @memberof cc.Updates
+     *
+     * @description
+     * Creates/Updates the invoice address for the user.
+     *
+     * @param {object} invoiceAddress Invoice address object.
      */
     self.updateInvoiceAddress = function(invoiceAddress){
         return storageService.set(STORE_INVOICE_ADDRESS_KEY, invoiceAddress);
     };
 
     /**
-     * Gets the shipping address for the user
+     * @method getShippingAddress
+     * @memberof cc.UserService
+     *
+     * @description
+     * Gets the shipping address for the user.
+     *
+     * @return {object} shipping address object.
      */
     self.getShippingAddress = function(){
         var address = storageService.get(STORE_SHIPPING_ADDRESS_KEY);
@@ -48,7 +74,13 @@ cc.define('cc.UserService', function(storageService, configService){
     };
 
     /**
-     * Creates/Updates the shipping address for the user
+     * @method updateShippingAddress
+     * @memberof cc.UserService
+     *
+     * @description
+     * Creates/Updates the shipping address for the user.
+     *
+     * @param {object} invoiceAddress
      */
     self.updateShippingAddress = function(invoiceAddress){
         return storageService.set(STORE_SHIPPING_ADDRESS_KEY, invoiceAddress);
