093b67d16925efbe465555e8e4c636d50f14bc41
Pascal Precht <pascal.precht@googlemail.com>
2014-01-06 16:16:19 +0100

    docs(cc.TrackingService): adds docs for cc.TrackingService

--- sdk/src/core/cc.trackingService.js
+++ sdk/src/core/cc.trackingService.js
@@ -1,9 +1,28 @@
+/**
+ * @name TrackingService
+ * @namespace cc.TrackingService
+ *
+ * @description
+ * Abstraction layer to communicate with concrete tracker services
+ * like Google Analytics.
+ */
 cc.define('cc.TrackingService', function($window, $http){
     'use strict';
 
     var self = {};
     var trackers = [];
 
+    /**
+     * @method addTracker
+     * @memberof cc.TrackingService
+     *
+     * @description
+     * Adds a concrete tracker service implementation and also takes care
+     * of the setup. It'll throw exceptions if the tracker service
+     * doesn't implement the needed API.
+     *
+     * @param {object} tracker Concrete tracker implementation.
+     */
     self.addTracker = function(tracker) {
 
         if (!tracker.setup){
@@ -19,6 +38,15 @@ cc.define('cc.TrackingService', function($window, $http){
         trackers.push(tracker);
     };
 
+    /**
+     * @method trackEvent
+     * @memberof cc.TrackingService
+     *
+     * @description
+     * Forces all registered trackers to track an event.
+     *
+     * @param {object} eventData Event data object.
+     */
     self.trackEvent = function(eventData) {
         trackers.forEach(function(tracker){
             tracker.trackEvent(eventData, $http);
