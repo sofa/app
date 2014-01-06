b17f50afead538bcdb81a025d8d5699289022b4f
Pascal Precht <pascal.precht@googlemail.com>
2014-01-06 16:04:42 +0100

    docs(cc.LocalStorageService): adds docs for cc.LocalStorageService
    kinda sorta

--- sdk/src/core/cc.sessionStorageService.js
+++ sdk/src/core/cc.sessionStorageService.js
@@ -1,4 +1,10 @@
-//we just wrap store.js in a service here
+/**
+ * @name LocalStorageService
+ * @namespace cc.LocalStorageService
+ *
+ * @description
+ * We just wrap store.js in a service here.
+ */
 cc.define('cc.LocalStorageService', function(){
     return store;
 });
