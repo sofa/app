3a055ed63cacf41199279f57c2a225c3095832db
Pascal Precht <pascal.precht@googlemail.com>
2014-01-06 15:05:17 +0100

    docs(cc.helper.TreeIterator): adds docs for TreeIterator

--- sdk/src/core/cc.helper.treeIterator.js
+++ sdk/src/core/cc.helper.treeIterator.js
@@ -1,12 +1,28 @@
-//We only use the TreeIterator to built a HashMap for fast lookups.
-//So it doesn't really care if we use a depth first or a breadth first approach.
+/**
+ * @name TreeIterator
+ * @namespace cc.helper.TreeIterator
+ *
+ * @description
+ * We only use the TreeIterator to built a HashMap for fast lookups.
+ * So it doesn't really care if we use a depth first or a breadth first approach.
+ */
 cc.define('cc.util.TreeIterator', function(tree, childNodeProperty){
 
     'use strict';
 
     var me = this,
         continueIteration = true;
-
+    
+    /**
+     * @method iterateChildren
+     * @memberof cc.helper.TreeIterator
+     *
+     * @description
+     * Iterates over a tree of children and applies a given function to
+     * each node.
+     *
+     * @param {function} fn Map function
+     */
     me.iterateChildren = function(fn){
         continueIteration = true;
         return _iterateChildren(tree, fn);
