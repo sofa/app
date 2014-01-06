02c1f82d9537f8c87cf3d672f2ed3c38b9482312
Pascal Precht <pascal.precht@googlemail.com>
2014-01-06 15:12:02 +0100

    docs(cc.models.BasketItem): adds docs for basket item model

--- sdk/src/core/cc.models.basketItem.js
+++ sdk/src/core/cc.models.basketItem.js
@@ -1,3 +1,11 @@
+/**
+ * @name BasketItem
+ * @namespace cc.models.BasketItem
+ *
+ * @description
+ * A basket item model that represents basket items. This model provides some methods
+ * to access information about the basket item such as the price or the variant id.
+ */
 cc.define('cc.models.BasketItem', function(){
 
     'use strict';
@@ -9,18 +17,54 @@ cc.define('cc.models.BasketItem', function(){
     return self;
 });
 
+/**
+ * @method getPrice
+ * @memberof cc.models.BasketItem
+ *
+ * @description
+ * Returns the price of the basket item depending on the variant.
+ *
+ * @return {float} Price
+ */
 cc.models.BasketItem.prototype.getPrice = function(){
     return this.variant && cc.Util.isNumeric(this.variant.price) ? this.variant.price : this.product.price;
 };
 
+/**
+ * @method getTotal
+ * @memberof cc.models.BasketItem
+ *
+ * @description
+ * Returns the total price of the basket item considering the quantity.
+ *
+ * @return {float} Total price
+ */
 cc.models.BasketItem.prototype.getTotal = function(){
     return cc.Util.round(this.quantity * this.getPrice(), 2);
 };
 
+/**
+ * @method getVariantID
+ * @memberof cc.models.BasketItem
+ *
+ * @description
+ * Returns the variant id of the basket item if it exists.
+ *
+ * @return {int} Variant id.
+ */
 cc.models.BasketItem.prototype.getVariantID = function(){
     return this.variant ? this.variant.variantID : null;
 };
 
+/**
+ * @method getOptionID
+ * @memberof cc.models.BasketItem
+ *
+ * @description
+ * Returns the option id of the basket item if it exists.
+ *
+ * @return {int} Option id.
+ */
 cc.models.BasketItem.prototype.getOptionID = function(){
     return cc.Util.isNumber(this.optionID) ? this.optionID : null;
 };
