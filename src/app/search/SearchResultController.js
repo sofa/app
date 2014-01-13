c9cf64c7d19925d94eefc7048775f64fa6c8e571
Pascal Precht <pascal.precht@googlemail.com>
2014-01-13 16:08:53 +0100

    chore(grunt): introduces new build process
    Here it is! Our new build process. This PR does the following:
    
    - introduces new tasks build, compile, compile-debug, deploy, deploy-debug
    - removes the unsed stable angular version
    - updates the .jshintrc
    - adds build.config.js
    - introduces a vendor folder (here's our custom angular version installed)
    - removes lib folder
    - introduces assets folder
    - Our new build process is splitted into two tasks build and compile.
    - moves files from `modules` to `src/app`
    - moves scss files under `assets` folder
    - makes grunt also watching vendor js files
    - moves grunt-conventional-changelog to `tasks` folder
    
    The build task generates a running app. This is for local development. It also
    starts a server for you so you don't have to care bout that :)
    
    The compile task takes the generated stuff from the build task and gives you a
    production ready app.

--- src/app/search/SearchResultController.js
+++ src/app/search/SearchResultController.js
@@ -1,35 +1,29 @@
+'use strict';
 
+angular.module('CouchCommerceApp').controller('SearchResultController', function SearchResultController($scope, searchService, searchUiState, navigationService) {
 
-angular
-    .module('CouchCommerceApp')
-    .controller('SearchResultController',
-    [
-        '$scope', 'searchService', 'searchUiState', 'navigationService',
-        function SearchResultController($scope, searchService, searchUiState, navigationService) {
+    var vm = this;
 
-            var vm = this;
+    vm.searchUiState = searchUiState;
+    vm.navigationService = navigationService;
 
-            vm.searchUiState = searchUiState;
-            vm.navigationService = navigationService;
+    vm.createGroupText = function (grouping) {
+        return grouping.items.length === 1 ?
+                '1 ' + $scope.ln.searchProductFoundIn + ' <b>' + grouping.groupText + '</b>' :
+                grouping.items.length + ' ' + $scope.ln.searchProductsFoundIn + ' <b>' + grouping.groupText + '</b>';
+    };
 
-            vm.createGroupText = function(grouping){
-                return grouping.items.length === 1 ?
-                       '1 ' + $scope.ln.searchProductFoundIn + ' <b>' + grouping.groupText + '</b>' :
-                       grouping.items.length + ' ' + $scope.ln.searchProductsFoundIn + ' <b>' + grouping.groupText + '</b>';
-            };
+    vm.goToCategory = function (result) {
+        searchUiState.closeSearch();
+        navigationService.navigateToCategory(result.groupKey);
+    };
 
-            vm.goToCategory = function(result){
-                searchUiState.closeSearch();
-                navigationService.navigateToCategory(result.groupKey);
-            };
+    vm.goToProduct = function (item) {
+        searchUiState.closeSearch();
+        navigationService.navigateToProduct({
+            categoryUrlId: item.categoryUrlKey,
+            urlKey: item.productUrlKey
+        });
+    };
 
-            vm.goToProduct = function(item){
-                searchUiState.closeSearch();
-                navigationService.navigateToProduct({
-                    categoryUrlId: item.categoryUrlKey,
-                    urlKey: item.productUrlKey
-                });
-            };
-
-        }
-    ]);
+});
