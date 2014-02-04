3cea2e7170cbfdb209925f6d42e67b62235a2494
Pascal Precht <pascal.precht@googlemail.com>
2014-02-04 21:14:11 +0100

    tests(app): adds e2e tests for snap menu
    finally we have simple (but proper) e2e tests for our snap menu

--- src/app/app.e2e.spec.js
+++ src/app/app.e2e.spec.js
@@ -8,4 +8,17 @@ describe('CouchCommerce App', function () {
 
         expect(element(by.className('cc-side-menu')).isDisplayed()).toBe(true);
     });
+    it('should open snap menu on click', function () {
+        browser.get('/');
+        element(by.className('fa-bars')).click();
+
+        expect(element(by.className('cc-side-menu')).isDisplayed()).toBe(true);
+        element(by.className('fa-bars')).click();
+        // browser.debugger();
+        browser.wait(function () {
+            return element(by.className('cc-side-menu')).isDisplayed().then(function (displayed) {
+                return !displayed;
+            });
+        }, 1000, 'menu should disappear');
+    });
 });
