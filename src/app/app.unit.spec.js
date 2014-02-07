828888b9dbb64652ec695279cff220b51d12bea6
Pascal Precht <pascal.precht@googlemail.com>
2014-02-07 11:53:42 +0100

    tests(app): adds some basic tests for the app

--- src/app/app.unit.spec.js
+++ src/app/app.unit.spec.js
@@ -2,9 +2,35 @@
 
 describe('CouchCommerceApp', function () {
 
+    var $rootScope;
+
     beforeEach(module('CouchCommerceApp'));
+
+    beforeEach(inject(function (_$rootScope_) {
+        $rootScope = _$rootScope_;
+    }));
 
     it('should run the tests', function () {
         expect(true).toBe(true);
+    });
+
+    it('should have i18n data defined on $rootScope', function () {
+        expect($rootScope.ln).toBeDefined();
+    });
+
+    it('should have config defined on $rootScope', function () {
+        expect($rootScope.cfg).toBeDefined();
+    });
+
+    it('should have sideDirectionService defined on $rootScope', function () {
+        expect($rootScope.slideDirectionService).toBeDefined();
+    });
+
+    it('should have template service defined on $rootScope', function () {
+        expect($rootScope.tpl).toBeDefined();
+    });
+
+    it('should have tablet size flag defined on $rootScope', function () {
+        expect($rootScope.isTabletSize).toBeDefined();
     });
 });
