c0f75f03cfa4e2d52a630408d1865175d3a257d5
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-02-20 16:23:48 +0100

    fix(cart): improve perf/reliability of plus/minus buttons
    Turns out basketService.getSummary() is quite costly.
    On some devices (iPhone, I'm looking at you) there's not enough
    time left in our budget to do the calculation within the given frame.
    This causes quite weird behaviour (dropped frames?) where the screen
    isn't updated and then when the user tappes the next time the amount of the
    item jumps two steps (or better that's how it looks for the user, in reality
    most likely it was just the screen which wasn't updated correctly before)
    We overcome this by scheduling the getSummary() for the next frame.

--- src/app/cart/CartController.js
+++ src/app/cart/CartController.js
@@ -1,4 +1,5 @@
 'use strict';
+/* globals requestAnimationFrame */
 
 angular.module('CouchCommerceApp')
 .controller('CartController', function ($scope, basketService, navigationService, checkoutService, configService, payPalOverlayService, dialog) {
@@ -8,16 +9,29 @@ angular.module('CouchCommerceApp')
     $scope.configService = configService;
 
     var updateModels = function () {
-        $scope.summary = basketService.getSummary();
 
-        //that's a bit of a hack. We use the total box for both cart
-        //and summary page. In the summary page we always have a server generated
-        //shipping. However, in the cart we might want to show a link to the
-        //shipping costs page. The total box checks for shipping === null to
-        //either show the link or the value.
-        if (configService.get('shippingCost') === null) {
-            $scope.summary.shipping = null;
-        }
+        // turns out basketService.getSummary() is quite costly.
+        // On some devices (iPhone, I'm looking at you) there's not enough
+        // time left in our budget to do the calculation within the given frame.
+        // This causes quite weird behaviour (dropped frames?) where the screen
+        // isn't updated and then when the user tappes the next time the amount of the
+        // item jumps two steps (or better that's how it looks for the user, in reality
+        // most likely it was just the screen which wasn't updated correctly before)
+        // We overcome this by scheduling the getSummary() for the next frame.
+        requestAnimationFrame(function () {
+            $scope.summary = basketService.getSummary();
+
+            //that's a bit of a hack. We use the total box for both cart
+            //and summary page. In the summary page we always have a server generated
+            //shipping. However, in the cart we might want to show a link to the
+            //shipping costs page. The total box checks for shipping === null to
+            //either show the link or the value.
+            if (configService.get('shippingCost') === null) {
+                $scope.summary.shipping = null;
+            }
+
+            $scope.$digest();
+        });
 
         $scope.items = basketService.getItems();
         $scope.isEmpty = $scope.items.length === 0;
