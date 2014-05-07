a1110aa04b248fc2dd749641023b316f46acd5cf
Pascal Precht <pascal.precht@googlemail.com>
2014-05-07 16:59:24 +0200

    feat(app): adds dynamic 404 meta tag for prerender.io
    this is done to make prerender.io aware of actual 404 sites,
    because single page applications usually have catch all routes
    
    this commit adds a meta tag when an url is requested that would
    actually result in a 404 site so prerender.io is informed about that
    
    more information: https://prerender.io/getting-started#404s

--- src/app/app.js
+++ src/app/app.js
@@ -179,7 +179,24 @@ angular.module('CouchCommerceApp', [
             screenIndex: screenIndexes.pages
         });
 
-    $urlRouterProvider.otherwise('/');
+    $stateProvider
+        .state('404', {
+            templateUrl: 'common/404/cc-404.tpl.html'
+        });
+
+    $urlRouterProvider.otherwise(function ($injector) {
+        // since we're generating HTML snapshots for search engines
+        // via prerender.io, we have to add this meta tag when the
+        // requested url actually returns a 404 error
+        // https://prerender.io/getting-started#404s
+        var meta = document.createElement('meta');
+        meta.setAttribute('name', 'prerender-status-code');
+        meta.setAttribute('content', '404');
+
+        document.getElementsByTagName('head')[0].appendChild(meta);
+        var $state = $injector.get('$state');
+        $state.transitionTo('404');
+    });
 })
 //just to kick off the services
 .run(['stateChangeService', 'viewClassService', 'backStepHighlightService', function () { } ])
