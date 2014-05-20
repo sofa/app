ff507200ea29368a207a21893f6a978f3355c023
Pascal Precht <pascal.precht@googlemail.com>
2014-05-20 10:16:32 +0200

    chore(app): removes duplicate metaService

--- src/app/app.js
+++ src/app/app.js
@@ -316,34 +316,4 @@ angular.module('CouchCommerceApp', [
             actionId: configService.get('bingActionId')
         }));
     }
-}])
-.factory('metaService', ['$rootScope', 'configService', function ($rootScope, configService) {
-
-    var meta = configService.get('meta');
-
-    $rootScope.meta = {
-        robots: meta.robots,
-        description: meta.description
-    };
-
-    return {
-        set: function (data) {
-            $rootScope.meta = {
-                robots: data.robots || meta.robots,
-                description: (function () {
-                    var description = data.description;
-                    if (data.description === '') {
-                        return description;
-                    }
-                    return (data.description || meta.description).replace(/<\/?[^>]+(>|$)/g, '');
-                }())
-            };
-        },
-        reset: function () {
-            $rootScope.meta = {
-                robots: meta.robots,
-                description: meta.description
-            };
-        }
-    };
 }]);
