8c60f35f39a1359a3a496dd71faa145e8e7c4ea1
Ingo Blenke <info@ingoblenke.de>
2014-07-23 11:21:50 +0200

    fix(cc-src-directive): only lazy load items that are in the viewport

--- src/app/common/ccSrcDirective.js
+++ src/app/common/ccSrcDirective.js
@@ -1,41 +1,38 @@
 'use strict';
 
 /* global Image */
+/* global document */
 
 /**
  *  Utilizes the sofa-image-resize-service to fill the src attribute of an image
  */
 
 angular.module('CouchCommerceApp')
-    .directive('ccSrc', function (imageResizeService, $q, configService) {
+    .directive('ccSrc', function (imageResizeService, $q) {
 
         var LOADING_CLASS = 'cc-img-loading';
 
-        var placeholderUrl = configService.get('mediaPlaceholder');
-
-        var getResizedImage = function (imageUrl, opts) {
-            return imageResizeService.resize(imageUrl, opts);
-        };
-
-        var checkImage = function (imageUrl, opts) {
+        var lazyLoading = function (imageUrl, opts) {
             var deferred = $q.defer(),
                 image = new Image(),
-                resizedUrl = getResizedImage(imageUrl, opts);
-
-            image.onerror = function () {
-                if (placeholderUrl) {
-                    deferred.resolve(getResizedImage(placeholderUrl, opts));
-                } else {
-                    deferred.resolve('');
-                }
-            };
+                resizedUrl = imageResizeService.resize(imageUrl, opts);
 
             image.onload = function () {
                 deferred.resolve(resizedUrl);
             };
+
             image.src = resizedUrl;
 
             return deferred.promise;
+        };
+
+        var elementInViewport = function (el) {
+            var rect = el.getBoundingClientRect();
+
+            return (
+                rect.top >= 0 && rect.left >= 0 &&
+                rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+            );
         };
 
         return {
@@ -47,19 +44,20 @@ angular.module('CouchCommerceApp')
                     parent = $element.parent(),
                     showSpinner = attrs.ccSrcSpinner;
 
-                if (showSpinner) {
-                    parent.addClass(LOADING_CLASS);
-                }
                 // TODO: enable this when image resizer is capable of force-dimensions
 //                attrs.$set('width', opts.maxwidth);
 //                attrs.$set('height', opts.maxheight);
 
-                checkImage(imageUrl, opts).then(function (url) {
-                    attrs.$set('src', url);
-                    if (showSpinner) {
+                if (showSpinner && elementInViewport($element[0])) {
+                    parent.addClass(LOADING_CLASS);
+                    lazyLoading(imageUrl, opts).then(function (imageUrl) {
                         parent.removeClass(LOADING_CLASS);
-                    }
-                });
+                        attrs.$set('src', imageUrl);
+                    });
+                } else {
+                    attrs.$set('src', imageResizeService.resize(imageUrl, opts));
+                }
+
             }
         };
     });
