54f4116ac6f5480ba6ec410d5ed7a5ff011ea95b
Ingo Blenke <info@ingoblenke.de>
2014-07-23 16:34:21 +0200

    fix(cc-src-directive): rmeove lazy loading completely
    it's not going to work with large listings...

--- src/app/common/ccSrcDirective.js
+++ src/app/common/ccSrcDirective.js
@@ -1,62 +1,24 @@
 'use strict';
 
-/* global Image */
-/* global document */
-
 /**
  *  Utilizes the sofa-image-resize-service to fill the src attribute of an image
  */
 
 angular.module('CouchCommerceApp')
-    .directive('ccSrc', function (imageResizeService, $q) {
-
-        var LOADING_CLASS = 'cc-img-loading';
-
-        var lazyLoading = function (imageUrl, opts) {
-            var deferred = $q.defer(),
-                image = new Image(),
-                resizedUrl = imageResizeService.resize(imageUrl, opts);
-
-            image.onload = function () {
-                deferred.resolve(resizedUrl);
-            };
-
-            image.src = resizedUrl;
-
-            return deferred.promise;
-        };
-
-        var elementInViewport = function (el) {
-            var rect = el.getBoundingClientRect();
-
-            return (
-                rect.top >= 0 && rect.left >= 0 &&
-                rect.top <= (window.innerHeight || document.documentElement.clientHeight)
-            );
-        };
+    .directive('ccSrc', function (imageResizeService) {
 
         return {
             restrict: 'A',
             link: function ($scope, $element, attrs) {
 
                 var imageUrl = $scope.$eval(attrs.ccSrc),
-                    opts = $scope.$eval(attrs.ccSrcConfig),
-                    parent = $element.parent(),
-                    showSpinner = attrs.ccSrcSpinner;
+                    opts = $scope.$eval(attrs.ccSrcConfig);
 
                 // TODO: enable this when image resizer is capable of force-dimensions
 //                attrs.$set('width', opts.maxwidth);
 //                attrs.$set('height', opts.maxheight);
 
-                if (showSpinner && elementInViewport($element[0])) {
-                    parent.addClass(LOADING_CLASS);
-                    lazyLoading(imageUrl, opts).then(function (imageUrl) {
-                        parent.removeClass(LOADING_CLASS);
-                        attrs.$set('src', imageUrl);
-                    });
-                } else {
-                    attrs.$set('src', imageResizeService.resize(imageUrl, opts));
-                }
+                attrs.$set('src', imageResizeService.resize(imageUrl, opts));
 
             }
         };
