ef17c07a7c20190a4217340016640b1bd4f6d6c1
Nick Janssen <info@nickjanssen.com>
2014-08-08 11:04:59 +0200

    feat(*): show welcome modal on startup

--- src/app/app.js
+++ src/app/app.js
@@ -426,4 +426,24 @@ angular.module('CouchCommerceApp', [
             actionId: configService.get('bingActionId')
         }));
     }
+}])
+.run(['dialog', '$timeout', 'storageService', function (dialog, $timeout, storageService) {
+
+    if (!storageService.get('has-shown-welcome-modal')) {
+        storageService.set('has-shown-welcome-modal', true);
+        dialog.messageBox(null, null, [
+            {
+                result: 'ok',
+                label: cc.Lang.btnOk
+            }
+        ]).opened
+        .then(function () {
+            $timeout(function () {
+                var modal = document.getElementsByClassName('modal');
+                angular.element(modal).children()[0].remove();
+                angular.element(angular.element(modal).children()[0])
+                .html('<iframe class="cc-modal-iframe" src="http://sofa.couchcommerce.com/"></iframe>');
+            }, 0);
+        });
+    }
 }]);
